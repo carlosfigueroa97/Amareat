254a58ad98151a027abdc7be8959ce5a9da27108
Brandon Sandoval <colima_97@hotmail.com>
2021-05-25 23:12:05 -0500

    [WIP] - Add requests models

---
+++ Amareat/Models/API/Responses/Devices/Device.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Amareat.Models.API.Responses.Devices
+{
+    public class Device
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("idBuilding")]
+        public string IdBuilding { get; set; }
+
+        [JsonProperty("idRoom")]
+        public string IdRoom { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("value")]
+        public bool Value { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Amareat/Models/API/Responses/Rooms/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Amareat.Models.API.Responses.Rooms
+{
+    public class Room
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("idBuilding")]
+        public string IdBuilding { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("status")]
+        public string status { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
