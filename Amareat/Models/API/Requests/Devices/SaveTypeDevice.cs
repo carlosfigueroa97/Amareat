1e41bfe9ff30a9dbf2e74f2ba3cd3cc2363fd004
Brandon Sandoval <colima_97@hotmail.com>
2021-05-26 00:13:24 -0500

    [Feature] - Add requests models

---
+++ Amareat/Models/API/Requests/Devices/SaveTypeDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Amareat.Models.API.Requests.Devices
+{
+    public class SaveTypeDevice
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+}
