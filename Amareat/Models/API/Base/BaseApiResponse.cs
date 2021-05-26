f5813d22b1436dcdadffad43bff35bd77181b1a0
Brandon Sandoval <colima_97@hotmail.com>
2021-05-25 19:09:49 -0500

    [WIP] - Add Base API responses

---
+++ Amareat/Models/API/Base/BaseApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Amareat.Models.API.Base
+{
+    public class BaseApiResponse
+    {
+        [JsonProperty("data")]
+        public string Data { get; set; }
+    }
+}
