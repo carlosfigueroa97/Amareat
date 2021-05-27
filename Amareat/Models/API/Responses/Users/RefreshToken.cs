81c4d68855f490d9ac1e5b4a6e95e4ebab44120b
Brandon Sandoval <colima_97@hotmail.com>
2021-05-27 13:01:49 -0500

    Add Responses files

---
+++ Amareat/Models/API/Responses/Users/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Amareat.Models.API.Responses.Users
+{
+    public class RefreshToken
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+}
