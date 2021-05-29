0872cb59185bf6f7310f13a631c896562c74a285
Carlos Figueroa <alex_bazan_1997@hotmail.com>
2021-05-28 19:46:06 -0500

    WIP - Added icons

--- Amareat/Helpers/Icons.cs
+++ Amareat/Helpers/Icons.cs
@@ -3,7 +3,16 @@ namespace Amareat.Helpers
 {
     public static class Icons
     {
-        // TODO:
-        public static string ArrowRight = "\uf105";
+        public static string ArrowRightIcon = "\uf105";
+        public static string ArrowLeftIcon = "\uf060";
+        public static string PlusIcon = "\uf067";
+        public static string EditIcon = "\uf044";
+        public static string CloseIcon = "\uf00d";
+        public static string PowerIcon = "\uf011";
+        public static string DownChevronIcon = "\uf078";
+        public static string RightChevronIcon = "\uf054";
+        public static string HomeIcon = "\uf015";
+        public static string CheckListIcon = "\uf46d";
+        public static string AccountIcon = "\uf2bd";
     }
 }
