205b158e8606e8a9bed90d23a2187b8ae9759e61
Carlos Figueroa <alex_bazan_1997@hotmail.com>
2021-05-30 04:05:37 -0500

    WIP - Added tabbedpage

---
+++ Amareat/Components/Views/Home/HomeView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Amareat.Components.Views.Home
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeView : TabbedPage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
