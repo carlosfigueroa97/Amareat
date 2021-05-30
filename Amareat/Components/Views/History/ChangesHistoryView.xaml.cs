dc4b60911617a734b3e7636fe6c7ac23f2959bf7
Brandon Sandoval <colima_97@hotmail.com>
2021-05-29 20:56:25 -0500

    [WIP] - Add basic view and initial properties

--- Amareat/Components/Views/History/ChangesHistoryView.xaml.cs
+++ Amareat/Components/Views/History/ChangesHistoryView.xaml.cs
@@ -10,6 +10,8 @@ namespace Amareat.Components.Views.History
         public ChangesHistoryView()
         {
             InitializeComponent();
+
+            BindingContext = new ChangesHistoryViewModel();
         }
     }
 }
--- Amareat/Components/Views/History/ChangesHistoryViewModel.cs
+++ Amareat/Components/Views/History/ChangesHistoryViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Amareat.Components.Base;
+﻿using System;
+using Amareat.Components.Base;
 
 namespace Amareat.Components.Views.History
 {
     public class ChangesHistoryViewModel : BaseVm
     {
+        #region Properties & Commands
+
+        public string DeviceName { get; set; }
+        public string Status { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+
+        #endregion
         public ChangesHistoryViewModel()
         {
         }
