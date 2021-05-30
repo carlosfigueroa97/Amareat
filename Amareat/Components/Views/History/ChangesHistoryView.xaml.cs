f45e937b6360a8c75791e3f03e5b930e33a19620
Brandon Sandoval <colima_97@hotmail.com>
2021-05-29 22:42:23 -0500

    [WIP] - Add properties and commands to use the APIHistory service

--- Amareat/Components/Views/History/ChangesHistoryView.xaml.cs
+++ Amareat/Components/Views/History/ChangesHistoryView.xaml.cs
@@ -7,11 +7,18 @@ namespace Amareat.Components.Views.History
 {
     public partial class ChangesHistoryView : ContentPage
     {
+        ChangesHistoryViewModel _vm =>
+            BindingContext as ChangesHistoryViewModel;
+
         public ChangesHistoryView()
         {
             InitializeComponent();
+        }
 
-            BindingContext = new ChangesHistoryViewModel();
+        protected override void OnAppearing()
+        {
+            _vm.GetHistoryDataCommand.Execute(null);
+            base.OnAppearing();
         }
     }
 }
--- Amareat/Components/Views/History/ChangesHistoryViewModel.cs
+++ Amareat/Components/Views/History/ChangesHistoryViewModel.cs
@@ -1,5 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Amareat.Components.Base;
+using HistoryApi = Amareat.Models.API.Responses.History;
+using Amareat.Services.Api.Interfaces;
+using Amareat.Services.Crash.Interfaces;
+using Amareat.Services.Localization.Interfaces;
+using MvvmHelpers.Commands;
 
 namespace Amareat.Components.Views.History
 {
@@ -7,14 +15,55 @@ namespace Amareat.Components.Views.History
     {
         #region Properties & Commands
 
-        public string DeviceName { get; set; }
+        private readonly ICrashReporting _crashReporting;
+        private readonly IHistoryService _historyService;
+
+        private CancellationToken cancellationToken =
+            new CancellationTokenSource().Token;
+
+        public Command GetHistoryDataCommand { get; set; }
+
+        public List<HistoryApi.History> HistoryListData { get; set; }
+        /*public string DeviceName { get; set; }
         public string Status { get; set; }
         public string User { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; }*/
 
         #endregion
-        public ChangesHistoryViewModel()
+
+        public ChangesHistoryViewModel(ICrashReporting crashReporting,
+            IHistoryService historyService)
         {
+            _crashReporting = crashReporting;
+            _historyService = historyService;
+
+            GetHistoryDataCommand = new Command(async () =>
+                await ExecuteGetHistoryDataCommand());
         }
+
+
+        #region Methods
+
+        async Task ExecuteGetHistoryDataCommand()
+        {
+            try
+            {
+                var apiResponse =
+                    await _historyService.GetHistory(cancellationToken);
+
+                if (apiResponse != null)
+                {
+                    HistoryListData = apiResponse.Data;
+                }
+
+                OnPropertyChanged(nameof(HistoryListData));
+            }
+            catch (Exception ex)
+            {
+                _crashReporting.TrackError(ex);
+            }
+        }
+
+        #endregion
     }
 }
