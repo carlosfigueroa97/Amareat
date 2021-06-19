6850f60c2a7edba37066a3a7b7d84342e26288dc
Brandon Sandoval <colima_97@hotmail.com>
2021-06-19 12:35:46 -0500

    [WIP] - Add properties and subscribe xaml items with the back

--- Amareat/Components/Popups/Building/AddedBuildingPopup.xaml.cs
+++ Amareat/Components/Popups/Building/AddedBuildingPopup.xaml.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using obj = System.Object;
 using System.Collections.Generic;
 using Amareat.Components.Popups.Base;
 using Xamarin.Forms;
+using txtEvent = Xamarin.Forms.TextChangedEventArgs;
 
 namespace Amareat.Components.Popups.Building
 {
     public partial class AddedBuildingPopup : BasePopupPage
     {
+        AddedBuildingViewModel vm => BindingContext as AddedBuildingViewModel;
+
         public AddedBuildingPopup()
         {
             InitializeComponent();
+            building.TextEntryChanged += BuildingEntryChanged;
         }
+
+        protected override void OnDisappearing()
+        {
+            building.TextEntryChanged -= BuildingEntryChanged;
+            base.OnDisappearing();
+        }
+
+        #region Methods
+
+        void BuildingEntryChanged(obj sender, txtEvent e) 
+        { 
+            var entry = sender as Entry;
+            vm.BuildingName = entry.Text;
+        }
+
+        #endregion
     }
 }
--- Amareat/Components/Popups/Building/AddedBuildingViewModel.cs
+++ Amareat/Components/Popups/Building/AddedBuildingViewModel.cs
@@ -1,11 +1,50 @@
 ﻿using Amareat.Components.Base;
+using Amareat.Services.Crash.Interfaces;
+using Amareat.Services.PopupNavigation.Interfaces;
+using MvvmHelpers.Commands;
 
 namespace Amareat.Components.Popups.Building
 {
     public class AddedBuildingViewModel : BaseVm
     {
-        public AddedBuildingViewModel()
+        #region Properties & Commands
+
+        #region Private Properties
+
+        private readonly IPopupNavigationService _popupNavigationService;
+        private readonly ICrashReporting _crashReporting;
+
+        private string _buildingName;
+        private bool _isListViewVisible;
+
+        #endregion
+
+        #region Public Properties
+
+        public Command ClosePopup { get; set; }
+
+        public string BuildingName
         {
+            get => _buildingName;
+            set => SetProperty(ref _buildingName, value);
+        }
+
+        public bool IsListViewVisible
+        {
+            get => _isListViewVisible;
+            set => SetProperty(ref _isListViewVisible, value);
+        }
+
+        #endregion
+
+        #endregion
+
+        public AddedBuildingViewModel(
+            IPopupNavigationService popupNavigationService,
+            ICrashReporting crashReporting)
+        {
+            _popupNavigationService = popupNavigationService;
+            _crashReporting = crashReporting;
         }
     }
 }
