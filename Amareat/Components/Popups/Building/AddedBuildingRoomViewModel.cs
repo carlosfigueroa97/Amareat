b2353914dbeb115096834a753812243b127d86b4
Brandon Sandoval <colima_97@hotmail.com>
2021-06-20 18:50:09 -0500

    [WIP] - Add ViewModel's base code

--- Amareat/Components/Popups/Building/AddedBuildingRoomViewModel.cs
+++ Amareat/Components/Popups/Building/AddedBuildingRoomViewModel.cs
@@ -1,11 +1,68 @@
-﻿using Amareat.Components.Base;
+﻿using System;
+using System.Threading.Tasks;
+using Amareat.Components.Base;
+using Amareat.Services.Crash.Interfaces;
+using Amareat.Services.PopupNavigation.Interfaces;
+using MvvmHelpers.Commands;
 
 namespace Amareat.Components.Popups.Building
 {
     public class AddedBuildingRoomViewModel : BaseVm
     {
-        public AddedBuildingRoomViewModel()
+        #region Properties & Commands
+        
+        #region Private Properties
+
+        private readonly IPopupNavigationService _popupNavigationService;
+        private readonly ICrashReporting _crashReporting;
+
+        private string _roomName;
+
+        #endregion
+
+        #region Public Properties
+
+        public Command ClosePopup { get; set; }
+
+        public string RoomName
         {
+            get => _roomName;
+            set => SetProperty(ref _roomName, value);
         }
+
+        #endregion
+
+        #endregion
+
+        public AddedBuildingRoomViewModel(
+            IPopupNavigationService popupNavigationService,
+            ICrashReporting crashReporting)
+        {
+            _popupNavigationService = popupNavigationService;
+            _crashReporting = crashReporting;
+
+            ClosePopup = new Command(async () =>
+                await ExecuteClosePopupCommand());
+        }
+
+        #region Public Methods
+
+        async Task ExecuteClosePopupCommand()
+        {
+            try
+            {
+                await _popupNavigationService.DismissPopupPage();
+            }
+            catch (Exception ex)
+            {
+                _crashReporting.TrackError(ex);
+            }
+
+        }
+
+        #endregion
+
+        #region Private Methods
+        #endregion
     }
 }
