e829663b6be74c8a382571140b37b20eaa74c376
Brandon Sandoval <colima_97@hotmail.com>
2021-06-20 18:49:07 -0500

    [WIP] - Add commands and minor fixes

--- Amareat/Components/Popups/Building/AddedBuildingViewModel.cs
+++ Amareat/Components/Popups/Building/AddedBuildingViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Amareat.Components.Base;
+﻿using System;
+using System.Threading.Tasks;
+using Amareat.Components.Base;
 using Amareat.Services.Crash.Interfaces;
 using Amareat.Services.PopupNavigation.Interfaces;
 using MvvmHelpers.Commands;
@@ -23,6 +25,8 @@ namespace Amareat.Components.Popups.Building
 
         public Command ClosePopup { get; set; }
 
+        public Command AddRoomCommand { get; set; }
+        
         public string BuildingName
         {
             get => _buildingName;
@@ -45,6 +49,44 @@ namespace Amareat.Components.Popups.Building
         {
             _popupNavigationService = popupNavigationService;
             _crashReporting = crashReporting;
+
+            ClosePopup = new Command(async () =>
+                await ExecuteClosePopupCommand());
+            AddRoomCommand = new Command(async () =>
+                await ExecuteAddRoomCommand());
         }
+
+        #region Public Methods
+
+        async Task ExecuteClosePopupCommand()
+        {
+            try
+            {
+                await _popupNavigationService.DismissPopupPage();
+            }
+            catch (Exception ex)
+            {
+                _crashReporting.TrackError(ex);
+            }
+
+        }
+
+        async Task ExecuteAddRoomCommand()
+        {
+            try
+            {
+                await _popupNavigationService.
+                    PresentPopupPage<AddedBuildingRoomViewModel>();
+            }
+            catch (Exception ex)
+            {
+                _crashReporting.TrackError(ex);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+        #endregion
     }
 }
