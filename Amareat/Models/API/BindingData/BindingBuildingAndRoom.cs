3ab0808c3691941b44003640f10c679a86cc7424
Brandon Sandoval <colima_97@hotmail.com>
2021-08-07 13:56:40 -0500

    [WIP] - Add New Model to bind data between two VM

---
+++ Amareat/Models/API/BindingData/BindingBuildingAndRoom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Amareat.Models.API.BindingData
+{
+    public class BindingBuildingAndRoom
+    {
+        public string BuildingName { get; set; }
+
+        public bool IsEditable { get; set; }
+    }
+}
