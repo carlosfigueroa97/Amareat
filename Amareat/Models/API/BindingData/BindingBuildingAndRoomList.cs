0f0db478c9f5e1d5946bf73b05903d8c870d3cc4
Brandon Sandoval <colima_97@hotmail.com>
2021-08-08 13:07:10 -0500

    [WIP] - Add Model

---
+++ Amareat/Models/API/BindingData/BindingBuildingAndRoomList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Amareat.Models.API.BindingData
+{
+    public class BindingBuildingAndRoomList
+    {
+        public ObservableCollection<BindingBuildingAndRoom> 
+            BindingBuildingList { get; set; }
+    }
+}
